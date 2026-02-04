fbaffcbb6eb680687b7cbe4d82b3e391a8dfb165
WendelLorenzi <wlorenzi17@gmail.cm>
2026-02-04 01:26:22 -0300

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,15 @@ import express from 'express';
 import router from './routes';
 import helmet from 'helmet';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import { errorMiddleware } from './middlewares/error';
 import { apiLimiter } from './middlewares/rateLimit';
 
 const app = express();
 
 app.use(express.json());
-app.use(apiLimiter)
+app.use(apiLimiter);
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
 app.use(errorMiddleware);
